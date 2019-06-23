3e39cb6039af22539f0fb422a604727c1d6da75d
M. Jameel Baig <mjameelbaig@hotmail.com>
2019-06-24 01:36:12 +1000

    Wrangler.py in

--- moviewrangler.py
+++ moviewrangler.py
@@ -1,22 +1,22 @@
-# #%%
+#%%
 import sys
 import pandas as pd
 import datetime
 from datetime import timedelta
 import json
 
-# #%%
+# %%
 year = sys.argv[1] 
 genre = sys.argv[2]
 
 datafile = pd.read_csv('data/dataregion.csv')
 
 
-# #%%
-# datafile['startYearEdited'] = datafile[datafile['startYear'].notna()]['startYear'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
-# #%%
-# # year = '2004-04-22'
-# # genre = 'Documentary'
+#%%
+datafile['startYearEdited'] = datafile[datafile['startYear'].notna()]['startYear'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
+#%%
+# year = '2004-04-22'
+# genre = 'Documentary'
 filtered = datafile[(datafile['startYearEdited'].notna()) &
                     (datafile['numVotes'].notna()) &
                     (datafile['averageRating'].notna()) &
@@ -38,8 +38,6 @@ filtered = filtered.sort_values('moviescore', ascending=False)
 
 filtered = filtered[:200]
 
-
-
 mylist = []
 for row in filtered.iterrows():
     r = row[1]
@@ -55,3 +53,6 @@ for row in filtered.iterrows():
     })
 print(json.dumps(mylist))
 # #%%
+
+
+#%%
